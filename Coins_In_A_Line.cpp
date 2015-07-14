74c03578e7dff4baaabb98e772747ef44bb886d5
zhaoqing <zhaoqing@papayamobile.com>
2015-07-14 14:45:11 +0800

    Coins_In_A_Line

---
+++ Coins_In_A_Line.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    /**
+     * @param n: an integer
+     * @return: a boolean which equals to true if the first player will win
+     */
+     bool firstWillWin(int n) {
+        // write your code here
+        return n%3 != 0;
+    }
+};
