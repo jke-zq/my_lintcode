7104681234da9f73c456b9468b102c2f4c69b554
zhaoqing <zhaoqing@papayamobile.com>
2015-07-17 19:33:18 +0800

    Segment_Tree_QueryII

---
+++ Segment_Tree_QueryII.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition of SegmentTreeNode:
+ * class SegmentTreeNode {
+ * public:
+ *     int start, end, count;
+ *     SegmentTreeNode *left, *right;
+ *     SegmentTreeNode(int start, int end, int count) {
+ *         this->start = start;
+ *         this->end = end;
+ *         this->count = count;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
+class Solution {
+public:
+    /**
+     *@param root, start, end: The root of segment tree and 
+     *                         an segment / interval
+     *@return: The count number in the interval [start, end] 
+     */
+    int query(SegmentTreeNode *root, int start, int end) {
+        // write your code here
+        if(root == nullptr || root->start > end || root->end < start){
+            return 0;
+        }
+        if(start <= root->start && end >= root->end){
+            return root->count;
+        }
+        int left = query(root->left, start, end);
+        int right = query(root->right, start, end);
+        return left + right;
+    }
+};
