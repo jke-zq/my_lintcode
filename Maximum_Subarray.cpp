54eb2b71d861f3d6c27d2d65ec5dea71498191ef
jke0zq <jke0zq@gmail.com>
2015-11-17 15:43:48 +0800

    goood codes

--- Maximum_Subarray.cpp
+++ Maximum_Subarray.cpp
@@ -6,19 +6,29 @@ public:
      */
     int maxSubArray(vector<int> nums) {
         // write your code here
-        int s = nums[0];
-        int result = nums[0];
-        for (int i = 1; i < nums.size(); ++i)
+        // int s = nums[0];
+        // int result = nums[0];
+        // for (int i = 1; i < nums.size(); ++i)
+        // {
+        //     if (s < 0 || s + nums[i] < 0)
+        //     {
+        //         s = nums[i];
+        //     }
+        //     else
+        //     {
+        //         s += nums[i];
+        //     }
+        //     result = max(result, s);
+        // }
+        // return result;
+        //good codes
+        int s = 0;
+        int result = INT_MIN;
+        for (const auto &n : nums)
         {
-            if (s < 0 || s + nums[i] < 0)
-            {
-                s = nums[i];
-            }
-            else
-            {
-                s += nums[i];
-            }
+            s += n;
             result = max(result, s);
+            s = max(s, 0);
         }
         return result;
     }
