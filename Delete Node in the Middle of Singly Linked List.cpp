1b17b9c670861c5f3e66417b2279bcc0b55590ab
jke0zq <jke0zq@gmail.com>
2015-11-23 11:20:30 +0800

    Delete Node in the Middle of Singly Linked List

---
+++ Delete Node in the Middle of Singly Linked List.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition of ListNode
+ * class ListNode {
+ * public:
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int val) {
+ *         this->val = val;
+ *         this->next = NULL;
+ *     }
+ * }
+ */
+class Solution {
+public:
+    /**
+     * @param node: a node in the list should be deleted
+     * @return: nothing
+     */
+    void deleteNode(ListNode *node) {
+        // write your code here
+        ListNode *node_to_delete = node->next;
+        node->val = node_to_delete->val;
+        node->next = node_to_delete->next;
+        delete node_to_delete;
+    }
+};
