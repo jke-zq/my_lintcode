c3766f9722a23f59c7f877f4ccc9e13a0318720f
jke0zq <jke0zq@gmail.com>
2016-01-05 22:47:41 +0800

    Simplify Path

---
+++ Simplify Path.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    /**
+     * @param path the original path
+     * @return the simplified path
+     */
+    string simplifyPath(string& path) {
+        // Write your code here
+        vector<string> names;
+        vector<string> tokens(move(split(path, '/')));
+        for (const auto &token : tokens)
+        {
+            if (token == ".." && !names.empty())
+            {
+                names.pop_back();
+            }
+            else if (token != ".." && token != "." && !token.empty())
+            {
+                names.emplace_back(token);
+            }
+        }
+        
+        return string("/").append(join(names, '/'));
+    }
+    
+    vector<string> split(const string &s, const char delim)
+    {
+        vector<string> tokens;
+        stringstream ss(s);
+        string token;
+        while(getline(ss, token, delim))
+        {
+            tokens.emplace_back(token);
+        }
+        return tokens;
+    }
+    
+    string join(const vector<string> &names, const char &delim)
+    {
+        string s;
+        if (!names.empty())
+        {
+            ostringstream ss;
+            string delim_str;
+            delim_str.insert(delim_str.begin(), delim);
+            copy(names.cbegin(), prev(names.cend()), ostream_iterator<string>(ss, delim_str.c_str()));
+            ss << names.back();
+            s = ss.str();
+        }
+        return s;
+    }
+};
