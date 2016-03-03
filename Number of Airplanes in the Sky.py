86a5b3090b196147a83370c1c70a5ae503e8eb69
jke-zq <jke0zq@gmail.com>
2016-03-03 23:54:50 +0800

    a-2:Scan Line

---
+++ Number of Airplanes in the Sky.py
@@ -0,0 +1,31 @@
+"""
+Definition of Interval.
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+
+class Solution:
+    # @param airplanes, a list of Interval
+    # @return an integer
+    def countOfAirplanes(self, airplanes):
+        # write your code here
+        airs = []
+        for interval in airplanes:
+            s, e = interval.start, interval.end
+            airs.append((s, 1))
+            airs.append((e, 0))
+        
+        airs.sort()
+        count = 0
+        ret = 0
+        for time, fly in airs:
+            if fly == 1:
+                count += 1
+            else:
+                count -= 1
+            ret = max(ret, count)
+        return ret
+                
