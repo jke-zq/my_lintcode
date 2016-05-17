cae1ee1636819794c5a65fe4a91456171927498d
jke-zq <jke0zq@gmail.com>
2016-05-17 17:29:12 +0800

    deque

--- Sliding Window Maximum.py
+++ Sliding Window Maximum.py
@@ -21,3 +21,30 @@ class Solution:
                     deque.pop(0)
                 
         return ret
+
+
+## Solution two:
+import collections
+class Solution:
+    """
+    @param nums: A list of integers.
+    @return: The maximum number inside the window at each moving.
+    """
+    def maxSlidingWindow(self, nums, k):
+        # write your code here
+        if not nums:
+            return []
+        deque = collections.deque()
+        # print deque
+        ret = []
+        for i in range(len(nums)):
+            # while deque and deque[-1] > i - k and nums[deque[-1]] < nums[i]:
+            while deque and deque[-1] < nums[i]:
+                deque.pop()
+            deque.append(nums[i])
+            if i >= k - 1:
+                ret.append(deque[0])
+                if deque[0] == nums[i - k + 1]:
+                    deque.popleft()
+                
+        return ret
