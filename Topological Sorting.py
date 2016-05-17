4105a99de73e95bf14b9f2795a5c491454c184bf
jke-zq <jke0zq@gmail.com>
2016-05-17 17:27:37 +0800

    graph bfs

--- Topological Sorting.py
+++ Topological Sorting.py
@@ -28,7 +28,7 @@ class Solution:
         ## init the queue to BFS
         queue = []
         for g in graph:
-            if dicts[g] == 0:
+            if dicts[g] == 0:   ## if g not in dicts, values will be int() default value
                 queue.append(g)
         ans = []
         while queue:
@@ -40,4 +40,46 @@ class Solution:
                     queue.append(nb)
         return ans
                 
+
+##solution two
+
+# Definition for a Directed graph node
+# class DirectedGraphNode:
+#     def __init__(self, x):
+#         self.label = x
+#         self.neighbors = []
+
+class Solution:
+    """
+    @param graph: A list of Directed graph node
+    @return: A list of integer
+    """
+    def topSort(self, graph):
+        # write your code here
+        if not graph:
+            return []
+        dicts = collections.defaultdict(int) # node:indegree
+        for g in graph:
+            dicts[g] += 0      ## needed for the dicts.keys()
+            for gnb in g.neighbors:
+                dicts[gnb] += 1
+        
+        ans = []
+        for g in dicts.keys():
+            if dicts[g] == 0:
+                ans.append(g)
+        length = len(ans)
+        start = 0
+        while start < length:
+            g = ans[start]
+            for nb in g.neighbors:
+                dicts[nb] -= 1
+                if dicts[nb] == 0:
+                    ans.append(nb)
+                    length += 1
+            start += 1
+                    
+        return ans
+                
+
                 
