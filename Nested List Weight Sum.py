6697b30d9972d88c404dbc8b0d459550806ea366
jke-zq <jke0zq@gmail.com>
2016-06-22 23:51:00 +0800

    Nested List Weight Sum.py

---
+++ Nested List Weight Sum.py
@@ -0,0 +1,43 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger(object):
+#    def isInteger(self):
+#        """
+#        @return {boolean} True if this NestedInteger holds a single integer,
+#        rather than a nested list.
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return {int} the single integer that this NestedInteger holds,
+#        if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        """
+#
+#    def getList(self):
+#        """
+#        @return {NestedInteger[]} the nested list that this NestedInteger holds,
+#        if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        """
+
+class Solution(object):
+    # @param {NestedInteger[]} nestedList a list of NestedInteger Object
+    # @return {int} an integer
+    def depthSum(self, nestedList):
+        # Write your code here
+        
+        def helper(lists, factor = 1):
+            if lists.isInteger():
+                return factor * lists.getInteger()
+            ans = 0
+            for v in lists.getList():
+                ans += helper(v, factor + 1)
+            return ans
+            
+        ans = 0
+        for v in nestedList:
+            ans += helper(v)
+        return ans
