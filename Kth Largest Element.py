805c17d5691d46cbd0f517b8c9dec06936ea9efc
jke-zq <jke0zq@gmail.com>
2016-06-24 23:57:09 +0800

    Kth Largest Element.py

--- Kth Largest Element.py
+++ Kth Largest Element.py
@@ -64,3 +64,59 @@ class Solution:
                 right = pivot - 1
         
         return None
+
+
+
+import random
+class Solution:
+    # @param k & A a integer and an array
+    # @return ans a integer
+    def kthLargestElement(self, k, A):
+        ## [5, 5, 5, 5, 5, 5]
+        
+        if not A:
+            return None
+        length = len(A)
+        if k > length or k < 1:
+            return None
+        left, right = 0, length - 1
+        while left <= right:
+            pivot = random.randint(left, right)
+            ## three colors
+            ## for testcase: [5, 5, 5, 5, 5, 5] with replication
+            target = A[pivot]
+            start, end = left, right
+            pre = start
+            while start <= end:
+                if A[start] > target:
+                    A[pre], A[start] = A[start], A[pre]
+                    pre += 1
+                    start += 1
+                elif A[start] == target:
+                    start += 1
+                else:
+                    A[end], A[start] = A[start], A[end]
+                    end -= 1
+            if pre + 1 <= k <= end + 1:
+                return A[pre]
+            elif pre + 1 > k:
+                right = pre - 1
+            else:
+                left = end + 1
+            
+            # target = A[pivot]
+            # A[right], A[pivot] = A[pivot], A[right]
+            # pre = left
+            # for i in range(left, right):
+            #     if A[i] > target:
+            #         A[pre], A[i] = A[i], A[pre]
+            #         pre += 1
+            # A[right], A[pre] = A[pre], A[right]
+            # if pre + 1 == k:
+            #     return A[pre]
+            # elif pre + 1 > k:
+            #     right = pre - 1
+            # else:
+            #     left = pre + 1
+        # raise Exception
+        return None
