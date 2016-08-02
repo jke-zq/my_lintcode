54236e54b6af1f844ee02024d2094320610e963a
jke-zq <jke0zq@gmail.com>
2016-08-03 00:00:33 +0800

    ip2int

--- ip2int.py
+++ ip2int.py
@@ -2,6 +2,7 @@
 
 def ip_to_int(ipstr):
     # values = [int(k) for k in ipstr.split('.')]
-    values = map(int, ipstr.split('.'))
-    return reduce(lambda x, y: (x << 8) + y, values, 0)
+    # values = map(int, ipstr.split('.'))
+    # return reduce(lambda x, y: (x << 8) + y, values, 0)
 
+    return reduce(lambda x, y: (x << 8) + int(y), ipstr.split('.'), 0)
