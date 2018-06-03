e28e6cf4580ce9e388b6007ac60ddada70c535a3
jke-zq <jke0zq@gmail.com>
2018-06-03 12:39:16 +0800

    Binary Tree Level Order Traversal

--- Binary Tree Level Order Traversal.py
+++ Binary Tree Level Order Traversal.py
@@ -14,10 +14,10 @@ class Solution:
     """
     def levelOrder(self, root):
         # write your code here
-        
+
         if not root:
             return []
-        
+
         queue = collections.deque()
         queue.append(root)
         ans = []
@@ -32,5 +32,59 @@ class Solution:
                     queue.append(node.right)
             ans.append(curLevel)
         return ans
-        
-        
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: A Tree
+    @return: Level order a list of lists of integer
+    """
+    def levelOrder(self, root):
+        # write your code here
+        # if root is None:
+        #     return []
+        # ans, queue = [], [root]
+        # index, length = 0, 1
+        # while index < length:
+        #     tmp = []
+        #     for i in range(index, length):
+        #         cur = queue[index]
+        #         tmp.append(cur.val)
+        #         if cur.left:
+        #             queue.append(cur.left)
+        #             length += 1
+        #         if cur.right:
+        #             queue.append(cur.right)
+        #             length += 1
+        #         index += 1
+        #     ans.append(tmp)
+        # return ans
+        def dfs(node, tmp, cur_level, max_level):
+            if node is None:
+                return
+            if cur_level == max_level:
+                tmp.append(node.val)
+                return
+            dfs(node.left, tmp, cur_level + 1, max_level)
+            dfs(node.right, tmp, cur_level + 1, max_level)
+
+        ans = []
+        if root is None:
+            return ans
+        max_level = 0
+        while True:
+            tmp = []
+            dfs(root, tmp, 0, max_level)
+            if tmp == []:
+                break
+            ans.append(tmp)
+            max_level += 1
+
+        return ans
+
