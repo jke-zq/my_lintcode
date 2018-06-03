0287889ccfd65eac16645822795349ee50c1e05d
jke-zq <jke0zq@gmail.com>
2018-06-03 13:13:57 +0800

    Validate Binary Search Tree

--- Validate Binary Search Tree.py
+++ Validate Binary Search Tree.py
@@ -9,32 +9,56 @@ class Solution:
     """
     @param root: The root of binary tree.
     @return: True if the binary tree is BST, or false
-    """  
+    """
     def isValidBST(self, root):
         # write your code here
-        
+
         ## traverse
         # def doCheck(node, minVal, maxVal):
         #     if not node:
         #         return True
         #     else:
         #         return minVal < node.val < maxVal and doCheck(node.left, minVal, node.val) and doCheck(node.right, node.val, maxVal)
-        
+
         # return doCheck(root, float('-inf'), float('inf'))
-        
+
         ## DC
+        # def helper(node):
+        #     if not node:
+        #         return float('inf'), float('-inf'), True
+        #     leftMin, leftMax, leftRet = helper(node.left)
+        #     rightMin, rightMax, rightRet = helper(node.right)
+        #     if not leftRet or not rightRet or node.val <= leftMax or node.val >= rightMin:
+        #         return 0, 0, False
+        #     retMin = min(leftMin, node.val)
+        #     retMax = max(rightMax, node.val)
+        #     return retMin, retMax, True
+
+        # # if not root:
+        # #     return True
+        # __, __, ret = helper(root)
+        # return ret
+    def isValidBST(self, root):
+        # write your code here
         def helper(node):
             if not node:
-                return float('inf'), float('-inf'), True
+                return None, None, True
             leftMin, leftMax, leftRet = helper(node.left)
             rightMin, rightMax, rightRet = helper(node.right)
-            if not leftRet or not rightRet or node.val <= leftMax or node.val >= rightMin:
+            # if not leftRet or not rightRet or node.val <= leftMax or node.val >= rightMin:
+            #     return 0, 0, False
+            # retMin = min(leftMin, node.val)
+            # retMax = max(rightMax, node.val)
+            if node.right and rightMin <= node.val:
                 return 0, 0, False
-            retMin = min(leftMin, node.val)
-            retMax = max(rightMax, node.val)
+            if node.left and leftMax >= node.val:
+                return 0, 0, False
+            if not leftRet or not rightRet:
+                return 0, 0, False
+            retMin = leftMin if node.left else node.val
+            retMax = rightMax if node.right else node.val
             return retMin, retMax, True
-            
-        if not root:
-            return True
+
         __, __, ret = helper(root)
         return ret
+
